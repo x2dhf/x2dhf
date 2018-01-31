d5ae4eb0f2576830ae46e24dc999d87472d02bd0
Susi Lehtola <susi.lehtola@gmail.com>
2018-01-31 18:10:52 +0200

    Support for G, H and I functions in Gaussian output.

--- src/rexponents.f90
+++ src/rexponents.f90
@@ -1,7 +1,7 @@
 ! ***************************************************************************
 ! *                                                                         *
 ! *   Copyright (C) 1996-2010 Jacek Kobus <jkob@fizyka.umk.pl>              *
-! *                                                                         *     
+! *                                                                         *
 ! *   This program is free software; you can redistribute it and/or modify  *
 ! *   it under the terms of the GNU General Public License version 2 as     *
 ! *   published by the Free Software Foundation.                            *
@@ -9,13 +9,13 @@
 ! ***************************************************************************
 ! ### rexponents ###
 !
-!     Reads exponents and contraction coefficients from 
-!     a GAUSSSIAN94 output obtained with gfinput keyword 
-! 
-!     Warning! 
+!     Reads exponents and contraction coefficients from
+!     a GAUSSIAN94 output obtained with gfinput keyword
+!
+!     Warning!
 !     Basis functions of g, h and higher symmertries are not allowed
 !     Uncontracted basis set must be used
-! 
+!
 subroutine rexponents(ibc,ib,istop)
   use params
   use commons8
@@ -26,13 +26,13 @@ subroutine rexponents(ibc,ib,istop)
   parameter (maxcf=100)
 
   real (PREC), dimension(maxcf) :: expon(maxcf)
-  real (PREC), dimension(maxcf,4) :: dcoef
+  real (PREC), dimension(maxcf,7) :: dcoef
 
   !  character*3 :: symlab
   character*8 :: symlab
 
   istop=0
-  
+
   !     read the centre number
   !  read(7,'(i3)',err=900) icent
   call inCardG(7,0)
@@ -43,9 +43,9 @@ subroutine rexponents(ibc,ib,istop)
   elseif (icent.eq.-9999) then
      return
   endif
-  
+
   !     read symmetry label and the number of exponents
-  
+
   do i=1,maxbasis
      call inCardG(7,0)
 !     read(7,1000,err=904) symlab, ncf
@@ -54,7 +54,7 @@ subroutine rexponents(ibc,ib,istop)
      if (symlab.ne.'****    ') then
         call inInt(ncf)
         if (ncf.gt.maxcf) then
-           print *,'r_exponents: too many primitive functions per single contracted gaussian; increase maxcf'	
+           print *,'r_exponents: too many primitive functions per single contracted gaussian; increase maxcf'
            stop
         endif
         !        if (symlab.eq.' S ') then
@@ -65,32 +65,32 @@ subroutine rexponents(ibc,ib,istop)
               call inFloat(dcoef(j,1))
               !              read(7,1010,end=950,err=904) expon(j),dcoef(j,1)
            enddo
-           !              define s-type gaussians		  
+           !              define s-type gaussians
            ibc=ibc+1
            do j=1,ncf
               ib=ib+1
               if (ib.gt.maxbasis) goto 990
               coeff(ib)=dcoef(j,1)
-              primexp(ib)=expon(j)		    
+              primexp(ib)=expon(j)
               ixref(ib)=ibc
               lprim(ib)=0
               mprim(ib)=0
            enddo
         elseif (symlab.eq.'sp      ') then
-           do j=1,ncf              
+           do j=1,ncf
 !              read(7,1010,end=950,err=904) expon(j),dcoef(j,1),dcoef(j,2)
               call inCardG(7,0)
               call inFloat(expon(j))
               call inFloat(dcoef(j,1))
               call inFloat(dcoef(j,2))
            enddo
-           !              define s-type gaussians		  
+           !              define s-type gaussians
            ibc=ibc+1
            do j=1,ncf
               ib=ib+1
               if (ib.gt.maxbasis) goto 990
               coeff(ib)=dcoef(j,1)
-              primexp(ib)=expon(j)		    		    
+              primexp(ib)=expon(j)
               ixref(ib)=ibc
               lprim(ib)=0
               mprim(ib)=0
@@ -100,9 +100,9 @@ subroutine rexponents(ibc,ib,istop)
               ibc=ibc+1
               do j=1,ncf
                  ib=ib+1
-                 if (ib.gt.maxbasis) goto 990			
+                 if (ib.gt.maxbasis) goto 990
                  coeff(ib)=dcoef(j,2)
-                 primexp(ib)=expon(j)		    		
+                 primexp(ib)=expon(j)
                  ixref(ib)=ibc
                  lprim(ib)=1
                  if (k.eq.1) mprim(ib)=+1
@@ -111,7 +111,7 @@ subroutine rexponents(ibc,ib,istop)
               enddo
            enddo
         elseif (symlab.eq.'p       ') then
-           do j=1,ncf              
+           do j=1,ncf
 !              read(7,1010,end=950,err=904) expon(j),dcoef(j,2)
               call inCardG(7,0)
               call inFloat(expon(j))
@@ -122,9 +122,9 @@ subroutine rexponents(ibc,ib,istop)
               ibc=ibc+1
               do j=1,ncf
                  ib=ib+1
-                 if (ib.gt.maxbasis) goto 990			
+                 if (ib.gt.maxbasis) goto 990
                  coeff(ib)=dcoef(j,2)
-                 primexp(ib)=expon(j)		    		
+                 primexp(ib)=expon(j)
                  ixref(ib)=ibc
                  lprim(ib)=1
                  if (k.eq.1) mprim(ib)=+1
@@ -132,35 +132,35 @@ subroutine rexponents(ibc,ib,istop)
                  if (k.eq.3) mprim(ib)= 0
               enddo
            enddo
-           
+
            !              define d-type orbitals (d0,d1,d-1,d2,d-2)
         elseif (symlab.eq.'d       ') then
-           do j=1,ncf              
+           do j=1,ncf
 !              read(7,1010,end=950,err=904) expon(j),dcoef(j,3)
               call inCardG(7,0)
               call inFloat(expon(j))
               call inFloat(dcoef(j,3))
            enddo
-           
+
            do k=1,5
               ibc=ibc+1
               do j=1,ncf
                  ib=ib+1
                  if (ib.gt.maxbasis) goto 990
                  coeff(ib)=dcoef(j,3)
-                 primexp(ib)=expon(j)		    		
+                 primexp(ib)=expon(j)
                  ixref(ib)=ibc
                  lprim(ib)=2
                  if (k.eq.1) mprim(ib)= 0
                  if (k.eq.2) mprim(ib)=+1
                  if (k.eq.3) mprim(ib)=-1
                  if (k.eq.4) mprim(ib)=+2
-                 if (k.eq.5) mprim(ib)=-2		
+                 if (k.eq.5) mprim(ib)=-2
               enddo
            enddo
            !              define f-type orbitals (f0,f1,f-1,f2,f-2,f3,f-3)`
         elseif (symlab.eq.'f       ') then
-           do j=1,ncf              
+           do j=1,ncf
               !              read(7,1010,end=950,err=904) expon(j),dcoef(j,4)
               call inCardG(7,0)
               call inFloat(expon(j))
@@ -172,7 +172,7 @@ subroutine rexponents(ibc,ib,istop)
                  ib=ib+1
                  if (ib.gt.maxbasis) goto 990
                  coeff(ib)=dcoef(j,4)
-                 primexp(ib)=expon(j)		    	
+                 primexp(ib)=expon(j)
                  ixref(ib)=ibc
                  lprim(ib)=3
                  if (k.eq.1) mprim(ib)= 0
@@ -184,8 +184,98 @@ subroutine rexponents(ibc,ib,istop)
                  if (k.eq.7) mprim(ib)=-3
               enddo
            enddo
+           !              define g-type orbitals (g0,g1,g-1,g2,g-2,g3,g-3,g4,g-4)`
+        elseif (symlab.eq.'g       ') then
+           do j=1,ncf
+              !              read(7,1010,end=950,err=904) expon(j),dcoef(j,4)
+              call inCardG(7,0)
+              call inFloat(expon(j))
+              call inFloat(dcoef(j,5))
+           enddo
+           do k=1,9
+              ibc=ibc+1
+              do j=1,ncf
+                 ib=ib+1
+                 if (ib.gt.maxbasis) goto 990
+                 coeff(ib)=dcoef(j,5)
+                 primexp(ib)=expon(j)
+                 ixref(ib)=ibc
+                 lprim(ib)=4
+                 if (k.eq.1) mprim(ib)= 0
+                 if (k.eq.2) mprim(ib)=+1
+                 if (k.eq.3) mprim(ib)=-1
+                 if (k.eq.4) mprim(ib)=+2
+                 if (k.eq.5) mprim(ib)=-2
+                 if (k.eq.6) mprim(ib)=+3
+                 if (k.eq.7) mprim(ib)=-3
+                 if (k.eq.8) mprim(ib)=+4
+                 if (k.eq.9) mprim(ib)=-4
+              enddo
+           enddo
+           !              define h-type orbitals (h0,h1,h-1,h2,h-2,h3,h-3,h4,h-4,h5,h-5)`
+        elseif (symlab.eq.'h       ') then
+           do j=1,ncf
+              !              read(7,1010,end=950,err=904) expon(j),dcoef(j,4)
+              call inCardG(7,0)
+              call inFloat(expon(j))
+              call inFloat(dcoef(j,6))
+           enddo
+           do k=1,11
+              ibc=ibc+1
+              do j=1,ncf
+                 ib=ib+1
+                 if (ib.gt.maxbasis) goto 990
+                 coeff(ib)=dcoef(j,6)
+                 primexp(ib)=expon(j)
+                 ixref(ib)=ibc
+                 lprim(ib)=4
+                 if (k.eq.1) mprim(ib)= 0
+                 if (k.eq.2) mprim(ib)=+1
+                 if (k.eq.3) mprim(ib)=-1
+                 if (k.eq.4) mprim(ib)=+2
+                 if (k.eq.5) mprim(ib)=-2
+                 if (k.eq.6) mprim(ib)=+3
+                 if (k.eq.7) mprim(ib)=-3
+                 if (k.eq.8) mprim(ib)=+4
+                 if (k.eq.9) mprim(ib)=-4
+                 if (k.eq.10) mprim(ib)=+5
+                 if (k.eq.11) mprim(ib)=-5
+              enddo
+           enddo
+           !              define i-type orbitals (i0,i1,i-1,i2,i-2,i3,i-3,i4,i-4,i5,i-5,i6,i-6)`
+        elseif (symlab.eq.'i       ') then
+           do j=1,ncf
+              !              read(7,1010,end=950,err=904) expon(j),dcoef(j,4)
+              call inCardG(7,0)
+              call inFloat(expon(j))
+              call inFloat(dcoef(j,7))
+           enddo
+           do k=1,13
+              ibc=ibc+1
+              do j=1,ncf
+                 ib=ib+1
+                 if (ib.gt.maxbasis) goto 990
+                 coeff(ib)=dcoef(j,7)
+                 primexp(ib)=expon(j)
+                 ixref(ib)=ibc
+                 lprim(ib)=4
+                 if (k.eq.1) mprim(ib)= 0
+                 if (k.eq.2) mprim(ib)=+1
+                 if (k.eq.3) mprim(ib)=-1
+                 if (k.eq.4) mprim(ib)=+2
+                 if (k.eq.5) mprim(ib)=-2
+                 if (k.eq.6) mprim(ib)=+3
+                 if (k.eq.7) mprim(ib)=-3
+                 if (k.eq.8) mprim(ib)=+4
+                 if (k.eq.9) mprim(ib)=-4
+                 if (k.eq.10) mprim(ib)=+5
+                 if (k.eq.11) mprim(ib)=-5
+                 if (k.eq.12) mprim(ib)=+6
+                 if (k.eq.13) mprim(ib)=-6
+              enddo
+           enddo
         else
-           print *,'r_exponents: symmetries g and higher are not allowed'
+           print *,'r_exponents: basis functions higher than i are not allowed'
            stop
         endif
      else
@@ -195,13 +285,11 @@ subroutine rexponents(ibc,ib,istop)
   enddo
 00900 istop=1
   return
-    
+
 00904 print *,'r_exponents: error encountered when reading gauss94.out'
-  stop 
+  stop
 00950 stop 'r_exponents: end of gauss94.out file encountered'
 00990 stop 'r_exponents: too many basis functions; increase maxbasis'
 01000 format(a3,2x,i2)
 01010 format(2x,e16.10,2x,e16.10,2x,e16.10,2x)
 end subroutine rexponents
-
-
