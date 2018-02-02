78194625562e4c005b48c815d03fac15025329f6
Susi Lehtola <susi.lehtola@gmail.com>
2018-02-02 16:15:16 +0200

    BLAS appears to work now.

--- src/blas.F90
+++ src/blas.F90
@@ -91,40 +91,29 @@ subroutine  scal(n,da,dx,ix)
 
 end subroutine scal
 
-
-!!     Multiplies the matrix DX(nr,nc) times the vector DV(nc) and stores
-!!     the result in the vector DY(nr) by means of calling DGEMV
-!!
-!      subroutine dgemv (trans,nr1,nc,alpha,dx,nr2,dv,incr1,beta,
-!     &     dvr,incr2)
-!      integer*4  incr1,incr2,inr,nc,nr1,nr2
-!      real*8  alpha,beta,dx(nr1,nc),dv(nc),dvr(nr1),s
-!      character*1 trans
-!
-!      do inr=1,nr1
-!         s=0.0_PREC
-!         do inc=1,nc
-!            s=s+dx(inr,inc)*dv(inc)
-!         enddo
-!         dvr(inr)=s
-!      enddo
-
-!      return
-!      end
-
-
 !     Multiplies the matrix DX(nr,nc) times the vector DV(nc) and stores
 !     the result in the vector DY(nr) (simplified version of DGEMV)
 !
 subroutine gemv (nr1,nc,dx,dv,dvr)
   use params
   implicit none
-  integer :: nc,nr1
+  integer :: nc, nr1
   real (PREC), dimension(nr1,*) :: dx
   real (PREC), dimension(*) :: dv,dvr
 
 #ifdef HAVE_BLAS
-  call dgemv(nr1,nc,dx,dv,dvr)
+  character :: trans = 'n'
+  integer :: M, N
+  double precision :: alpha = 1.0, beta = 0.0
+  integer :: lda
+  integer :: incx = 1, incy = 1
+
+  ! Matrix size
+  M = nr1
+  N = nc
+  lda = M
+
+  call dgemv(trans, M, N, alpha, dx, lda, dv, incx, beta, dvr, incy)
 #else
   integer :: inc, inr
   real (PREC) :: s
