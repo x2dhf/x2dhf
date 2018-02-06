49c8705f2802d8db529489d1f1c041801c0f5bb9
Susi Lehtola <susi.lehtola@gmail.com>
2018-02-06 12:32:54 +0200

    Add an example target

--- compile.sh
+++ compile.sh
@@ -4,8 +4,10 @@
 export FC=gfortran
 # Compiler flags to use
 export FFLAGS="-O2 -Wall"
-# Installation directory (x2dhf will be installed under bin/)
-export target=$(pwd)
+
+# Installation directory
+export target=$(pwd) # this installs x2dhf under bin/ in the present directory
+#export target=${HOME}  # this installs x2dhf in ${HOME}/bin
 
 # Number of parallel processes in build
 export npar=4
@@ -21,4 +23,3 @@ cmake .. \
       -DCMAKE_Fortran_FLAGS_RELEASE:STRING="-DNDEBUG"
 make -j ${npar} install VERBOSE=1
 cd ..
-
